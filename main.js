f8ce90d77f11dfd6661e3832a56fee0ea0d859d9
Patrick Vicente <patrickvicenteph@gmail.com>
2023-02-11 17:20:53 +0400

    create about and contact feature

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-document.getElementById("contact-link").addEventListener("click", function() {
-    document.getElementById("contact-container").style.display = "block";
-  });
+const contactContainer = document.getElementById('about-me-contact'); // Get the element with id "about-me-contact"
+const contactLink = document.getElementById("contact-link"); // Get the element with id "contact-link"
+const aboutMeButton = document.getElementById('btn-about-me'); // Get the element with id "btn-about-me"
+
+// Function to toggle the visibility of contactContainer
+function toggleContactContainer() {
+
+
+  if (contactContainer.style.display === 'none') {
+    contactContainer.style.display = "flex";
+  } else {
+    contactContainer.style.display = "none"
+  }
+}
+
+contactLink.addEventListener("click", function(event) { // Add click event listener to contactLink
+  event.preventDefault(); // Prevent the default behavior of the link
+  toggleContactContainer(); // Call the toggleContactContainer function
+});
+
+aboutMeButton.addEventListener("click", function(event) { // Add click event listener to aboutMeButton
+  event.preventDefault();
+  toggleContactContainer(); // Call the toggleContactContainer function
+});
+
